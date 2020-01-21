1cc29d2604b359f67b8c5084d388e12e3dcac4c3
joethorley <joe@poissonconsulting.ca>
2020-01-21 10:59:12 -0800

    dims man

--- R/dims.R
+++ R/dims.R
@@ -1,10 +1,9 @@
 #' Dimensions of an Object
 #'
 #' Gets the dimensions of an object.
-#' Unlike `base::dim()`, dims works as intuitively expected for vectors and data.frames.
+#' Unlike `base::dim()`, dims works as intuitively expected 
+#' for vectors and data.frames.
 #' 
-#' @section Methods:
-#' To see all available methods use [utils::methods()].
 #' @inheritParams params
 #' @return An integer vector of the dimensions.
 #' @seealso [base::dim()]
