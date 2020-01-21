ae5e3c05bfdac7bce6cc9a32c7a89149e9dadc9b
joethorley <joe@poissonconsulting.ca>
2020-01-21 13:15:19 -0800

    man pars

--- R/pars.R
+++ R/pars.R
@@ -2,9 +2,6 @@
 #'
 #' Gets character vector of parameter names.
 #'
-#' @section Methods:
-#' To see all available methods use [utils::methods()].
-#'
 #' @inheritParams params
 #' @return A character vector of the parameter names.
 #' @export
