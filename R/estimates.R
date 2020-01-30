31d5917737d90bc3ea7193ba26be1e976afef82a
joethorley <joe@poissonconsulting.ca>
2020-01-30 08:06:08 -0800

    add estimates

---
+++ R/estimates.R
@@ -0,0 +1,11 @@
+#' Estimates
+#'
+#' Calculates the estimates for an MCMC object.
+#' 
+#' @inheritParams params
+#' @return An (unsorted) named (by parameters) list of numeric atomic objects of the estimates or an (unsorted) \code{\link[nlist]{nlist}}.
+#' @family {MCMC manipulations}
+#' @export
+estimates <- function(x, ...) {
+  UseMethod("estimates")
+}
