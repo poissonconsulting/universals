aa52fdbc746391b7686c97517caadf162b32b1cb
joethorley <joe@poissonconsulting.ca>
2020-01-29 18:48:55 -0800

    remove npdims_terms and pdims_terms

--- R/npdims-terms.R
+++
@@ -1,9 +0,0 @@
-#' Term Number of Parameter Dimensions
-#'
-#' Gets the number of dimensions of each parameter for each term.
-#'
-#' @inheritParams params
-#' @return A named list of integer vectors of the dimensions of each parameter for each term.
-#' @family {dimensions}
-#' @export
-npdims_terms <- function(x, ...) UseMethod("npdims_terms")
--- R/pdims-terms.R
+++
@@ -1,9 +0,0 @@
-#' Term Parameter Dimensions
-#'
-#' Gets the dimensions of each parameter for each term.
-#'
-#' @inheritParams params
-#' @return A named integer vector of the number of dimensions of each parameter for each term.
-#' @family {dimensions}
-#' @export
-pdims_terms <- function(x, ...) UseMethod("pdims_terms")
