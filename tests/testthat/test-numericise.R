1aab5eb19371bac3a57d32b2801dc2e9b90c4c01
joethorley <joe@poissonconsulting.ca>
2020-03-31 08:59:00 -0700

    - Moved numericise() generic to extras.

--- tests/testthat/test-numericise.R
+++
@@ -1,9 +0,0 @@
-test_that("numericise", {
-  numericise.logical <- function(x, ...) as.integer(x)
-  expect_identical(numericise(c(TRUE, FALSE, NA)), c(1L, 0L, NA_integer_))
-})
-
-test_that("numericize", {
-  numericise.logical <- function(x, ...) as.integer(x)
-  expect_identical(numericize(c(TRUE, FALSE, NA)), c(1L, 0L, NA_integer_))
-})
