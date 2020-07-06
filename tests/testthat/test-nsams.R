b5adc61f4986ce29d4e62e0764bf54063874afee
Joe Thorley <joe@poissonconsulting.ca>
2020-07-06 16:01:44 -0700

    starting nlist tests

--- tests/testthat/test-nsams.R
+++ tests/testthat/test-nsams.R
@@ -1,4 +1,21 @@
-test_that("nsams works", {
-  expect_identical(nsams(1L), 2L)
-  expect_identical(nsams(3:4), 4L)
+test_that("nsams.nlist::nlist", {
+  expect_identical(nsams(nlist::nlist()), 0L)
+  expect_identical(nsams(nlist::nlist(x = 1)), 1L)
+  expect_identical(nsams(nlist::nlist(x = 2:3)), 2L)
+  expect_identical(nsams(nlist::nlist(x = 2:3, y = 1)), 3L)
+  expect_identical(nsams(nlist::nlist(x = 2:3, y = matrix(1:9))), 11L)
+})
+
+test_that("nsams.nlist::nlists", {
+  expect_identical(nsams(nlist::nlists()), 0L)
+  expect_identical(nsams(nlist::nlists(nlist::nlist())), 0L)
+
+  expect_identical(nsams(nlist::nlists(nlist::nlist(x = 1))), 1L)
+  expect_identical(nsams(nlist::nlists(nlist::nlist(x = 2:3))), 2L)
+  expect_identical(nsams(nlist::nlists(nlist::nlist(x = 2:3, y = 1))), 3L)
+  expect_identical(nsams(nlist::nlists(nlist::nlist(x = 2:3, y = matrix(1:9)))), 11L)
+  expect_identical(nsams(nlist::nlists(
+    nlist::nlist(x = 2:3, y = matrix(1:9)),
+    nlist::nlist(x = 2:3, y = matrix(1:9))
+  )), 22L)
 })
