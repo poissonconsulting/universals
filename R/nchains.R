777401efeab1632d9099abff48f6b306dd1ac110
Joe Thorley <joe@poissonconsulting.ca>
2020-07-07 14:01:59 -0700

    examples nchains and split-chains

--- R/nchains.R
+++ R/nchains.R
@@ -7,6 +7,14 @@
 #' @return An integer scalar of the number of chains.
 #' @family MCMC dimensions
 #' @export
+#' @examplesIf requireNamespace("nlist", quietly = TRUE)
+#' library(nlist)
+#'
+#' nchains(nlist(x = 1:2))
+#' nlists <- nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7)))
+#' nchains(nlists)
+#' nchains(split_chains(nlists))
+#' @export
 nchains <- function(x, ...) {
   UseMethod("nchains")
 }
--- R/split-chains.R
+++ R/split-chains.R
@@ -8,6 +8,12 @@
 #' @return The modified object.
 #' @family MCMC manipulations
 #' @export
+#' @examplesIf requireNamespace("nlist", quietly = TRUE)
+#' library(nlist)
+#'
+#' nlists <- nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7)))
+#' nchains(nlists)
+#' nchains(split_chains(nlists))
 split_chains <- function(x, ...) {
   UseMethod("split_chains")
 }
