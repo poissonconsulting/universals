9717d3119cf6f6e3a7009cc3bfd6984bb2db5c61
Joe Thorley <joe@poissonconsulting.ca>
2020-07-07 14:18:51 -0700

    examples nterms, pars, pdims

--- R/nterms.R
+++ R/nterms.R
@@ -7,6 +7,16 @@
 #' @return A integer scalar of the number of terms.
 #' @family MCMC dimensions
 #' @export
+#' @examplesIf requireNamespace("nlist", quietly = TRUE)
+#' library(nlist)
+#'
+#' nterms(nlist(x = 2))
+#' nterms(nlist(x = NA_real_))
+#' nterms(nlist(x = 3, zz = matrix(2:5, 2)))
+#' nterms(nlists(
+#'   nlist(y = 3, zz = matrix(2:5, 2)),
+#'   nlist(y = 5, zz = matrix(1:4, 2))
+#' ))
 nterms <- function(x, ...) {
   UseMethod("nterms")
 }
--- R/pdims.R
+++ R/pdims.R
@@ -6,4 +6,9 @@
 #' @return A named list of integer vectors of the dimensions of each parameter.
 #' @family dimensions
 #' @export
+#' @examplesIf requireNamespace("nlist", quietly = TRUE)
+#' library(nlist)
+#'
+#' pdims(nlist(x = 1:3))
+#' pdims(nlist(y = 3, zz = matrix(2:5, 2)))
 pdims <- function(x, ...) UseMethod("pdims")
