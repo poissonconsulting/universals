a3b2a4dc7fbd88dc77a76f812d2ad5ad8f9c54c7
Joe Thorley <joe@poissonconsulting.ca>
2020-07-07 13:48:13 -0700

    example dims

--- R/dims.R
+++ R/dims.R
@@ -9,25 +9,26 @@
 #' @seealso [base::dim()]
 #' @family dimensions
 #' @export
+#' @examples
+#' dims(numeric(0))
+#' dims(1:3)
+#' dims(factor("a"))
+#' dims(matrix(1:4, nrow = 2L))
+#' dims(array(1:9, dim = c(3L,1L,3L)))
+#' dims(ToothGrowth)
+#' dims(Titanic)
 dims <- function(x, ...) {
   UseMethod("dims")
 }
 
 #' @rdname dims
 #' @export
-#' @examples
-#' dims(character(0))
-#' dims(1:3)
-#' dims(ToothGrowth)
-#' dims(Titanic)
 dims.default <- function(x, ...) {
   if (is.vector(x)) length(x) else dim(x)
 }
 
 #' @rdname dims
 #' @export
-#' @examples
-#' dims(factor("a"))
 dims.factor <- function(x, ...) {
   length(x)
 }
