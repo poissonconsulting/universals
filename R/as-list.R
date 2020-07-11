397747f66583073c0324ecc7a6f6200b077ba895
Joe Thorley <joe@poissonconsulting.ca>
2020-07-10 17:48:18 -0700

    - Added as_list().

---
+++ R/as-list.R
@@ -0,0 +1,22 @@
+#' As List
+#'
+#' Coerces an object to an list.
+#' All attributes are removed except any names.
+#'
+#' @inheritParams params
+#' @return A list.
+#' @export
+as_list <- function(x, ...) {
+  UseMethod("as_list")
+}
+
+#' @rdname as_list
+#' @export
+#' @examples
+#' as_list(1:3)
+#' as_list(c(x = 1, y = 2))
+as_list.default <- function(x, ...) {
+  x <- as.list(x)
+  attributes(x) <- NULL
+  x
+}
---
+++ tests/testthat/test-as-list.R
@@ -0,0 +1,4 @@
+test_that("as_list works", {
+  expect_identical(as_list(1:2), list(1L, 2L))
+  expect_identical(as_list(c(x = 1L, y = 2L)), list(1L, 2L))
+})
